3304e4df19c1f441818857d62e816179928ba164
006Darsh <21ce006@charusat.edu.in>
2024-02-01 17:03:47 +0530

    first commit of e-commerce

---
+++ ecommerce-frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage></SignUpPage>,
+  },
+]);
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
